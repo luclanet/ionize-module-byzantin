a37152521ed52f09f76e16288e6c46b3994be0a9
luclanet <claudio.mulas@lucla.biz>
2013-04-28 03:01:49 +0300

    Create byzantin_lang.php
    Added italian language on module!

---
+++ Byzantin/language/it/byzantin_lang.php
@@ -0,0 +1,39 @@
+<?php
+
+$lang['module_demo_title'] = 'TraduttoreByzantin';
+$lang['module_demo_about'] = 'Traduci il backend di Ionize';
+
+// Menu items
+$lang['module_byzantin_menu_source'] = 'Selezione Sorgente';
+$lang['module_byzantin_menu_destination'] = 'Seleziona Destinazione';
+$lang['module_byzantin_menu_file'] = 'Seleziona o carica un file';
+$lang['module_byzantin_menu_translate'] = 'Traduci';
+$lang['module_byzantin_menu_save'] = 'Salva';
+
+// Warning / Errors
+$lang['module_byzantin_post_max_vars'] = 'Non puoi tradurre questo file:<br />Per questo file sono necessarie almeno <strong>%s</strong> variabili POST.<br/>Il tuo server è protetto e non può accettare più di <strong>%s valori POST</strong>.<br/><br/>Modifica le tue impostazioni nel file <strong>php.ini</strong> e/o <strong>suhosin.ini</strong> e imposta i seguenti valori: <br/>max_input_vars = 8000<br/>suhosin.request.max_vars = 8000<br/>suhosin.post.max_vars = 8000</strong>';
+$lang['module_byzantin_translation_same'] = 'uguale alla sorgente';
+
+//
+$lang['module_byzantin_source'] = 'Sorgente';
+$lang['module_byzantin_destination'] = 'Destinazione';
+$lang['module_byzantin_upload_files'] = 'Carica files';
+$lang['module_byzantin_upload_drop_files_here'] = 'Drag & Drop i tuoi file php qui';
+$lang['module_byzantin_upload_success_message'] = 'File caricati con successo!';
+$lang['module_byzantin_upload_error_no_file_message'] = 'Prego seleziona un file';
+$lang['module_byzantin_upload_error_message'] = 'Errore durante l\'upload';
+$lang['module_byzantin_select_file_to_translate'] = 'Seleziona il file da tradurre';
+$lang['module_byzantin_upload_files_help'] = "I tuoi file caricati verranno sovrascritti se già esistenti.";
+$lang['module_byzantin_file'] = 'File';
+$lang['module_byzantin_key'] = 'Chiave';
+$lang['module_byzantin_translated'] = 'Tradotto';
+$lang['module_byzantin_existing_destination_folders'] = 'Cartelle di destinazione esistenti';
+$lang['module_byzantin_not_existing_destination_folders'] = 'Cartelle di destinazione non esistenti';
+$lang['module_byzantin_file_not_found_in'] = 'File non trovati in';
+$lang['module_byzantin_save_translation'] = 'Salva Traduzioni';
+$lang['module_byzantin_translation_file_saved'] = 'File Traduzioni salvate';
+$lang['module_byzantin_translation_file_not_saved'] = 'File Traduzioni non salvate';
+$lang['module_byzantin_filter_all'] = 'Tutte';
+$lang['module_byzantin_filter_empty'] = 'Traduzioni vuote';
+$lang['module_byzantin_filter_same'] = 'Uguali alla Sorgente';
+
